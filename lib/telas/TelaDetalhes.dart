cf677fe08af941d1e2ca60ca3ca166e60c2968fe
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2024-08-24 21:30:00 -0300

    alterado nome da variavel de email, rescrito trechos do código  para correção

--- lib/telas/TelaDetalhes.dart
+++ lib/telas/TelaDetalhes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:navigator_project/componentes/componenteAppBar.dart';
+import 'package:navigator_project/componentes/componenteElevatedButton.dart';
 import 'package:navigator_project/componentes/componenteMenu.dart';
 
 class TelaDetalhes extends StatefulWidget {
@@ -9,106 +11,109 @@ class TelaDetalhes extends StatefulWidget {
 
 class _TelaDetalhesState extends State<TelaDetalhes> {
 
-  late String? email='';
+  late String? usuario_email='';
   late String? protocolo='';
 
   @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero, () {
-      //final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
-      setState(() {
-
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    var formulario = GlobalKey<FormState>();
     final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
     setState(() {
-      email = arguments['email'];
+      usuario_email = arguments['usuario_email'];
       protocolo = arguments['protocolo'];
     });
     return Scaffold(
-      drawer: ComponenteMenu(email: email,),
-      appBar: ComponenteAppBar(tituloComponente: 'Detalhes',mostrarIconeMenu: false,usuarioLogado: email,),
+      drawer: ComponenteMenu(
+        email: usuario_email,
+      ),
+      appBar: ComponenteAppBar(
+        tituloComponente: 'Detalhes',
+        mostrarIconeMenu: false,
+        usuarioLogado: usuario_email,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            TextFormField(
-              initialValue: protocolo,
-              decoration: const InputDecoration(
-                labelText: 'Protocolo',
-              ),
-              readOnly: true,
+            SizedBox(height: 10,),
+            Text(
+               'Protocolo: $protocolo',
+              style: const TextStyle(fontWeight: FontWeight.bold),
             ),
-            TextFormField(
-              initialValue: arguments['date'],
-              decoration: const InputDecoration(
-                labelText: 'Data Sugestão',
-              ),
-              readOnly: true,
+            SizedBox(height: 10,),
+            Text(
+                'Data de Cadastro: ${DateFormat('dd/MM/yyyy HH:mm')
+                    .format(arguments['data_cadastro']
+                    .toDate()
+                )}'
             ),
-            TextFormField(
-              initialValue: arguments['status'],
-              decoration: const InputDecoration(
-                labelText: 'Status',
-              ),
-              readOnly: true,
+            SizedBox(height: 10,),
+            Text('Status: ${arguments['status']}'),
+            SizedBox(height: 10,),
+            Form(
+              key: formulario,
+                child:
+                Column(children: [
+                  TextFormField(
+                    initialValue: arguments['titulo'],
+                    decoration: const InputDecoration(
+                      labelText: 'Título',
+                    ),
+                    readOnly: true,
+                  ),
+                  TextFormField(
+                    initialValue: arguments['descricao'],
+                    decoration: const InputDecoration(
+                      labelText: 'Descrição',
+                    ),
+                    readOnly: true,
+                    maxLines: 3,
+                  ),
+                  TextFormField(
+                    initialValue: arguments['solucao_proposta'],
+                    decoration: const InputDecoration(
+                      labelText: 'Solução Proposta',
+                    ),
+                    readOnly: true,
+                    maxLines: 3,
+                  ),
+                  TextFormField(
+                    initialValue: arguments['beneficios'],
+                    decoration: const InputDecoration(
+                      labelText: 'Benefícios de implementação',
+                    ),
+                    readOnly: true,
+                    maxLines: 3,
+                  )
+                ],
+                ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ComponenteElevatedButton(
+                    formKey: formulario,
+                    corDoBotao: Colors.green,
+                    tituloBotao: 'Editar',
+                    funcao: (){
 
-            TextFormField(
-              initialValue: arguments['title'],
-              decoration: const InputDecoration(
-                labelText: 'Título',
-              ),
-              readOnly: true,
-            ),
-            TextFormField(
-              initialValue: arguments['description'],
-              decoration: const InputDecoration(
-                labelText: 'Descrição',
-              ),
-              readOnly: true,
-              maxLines: 3,
-            ),
-            TextFormField(
-              initialValue: arguments['proposal'],
-              decoration: const InputDecoration(
-                labelText: 'Solução Proposta',
-              ),
-              readOnly: true,
-              maxLines: 3,
-            ),
-            TextFormField(
-              initialValue: arguments['benefits'],
-              decoration: const InputDecoration(
-                labelText: 'Benefícios de implementação',
-              ),
-              readOnly: true,
-              maxLines: 3,
-            ),
-            TextFormField(
-              initialValue: arguments['feedback'],
-              decoration: const InputDecoration(
-                labelText: 'Feedback Recebido',
-              ),
-              readOnly: true,
-              maxLines: 3,
-            ),
+                    }
+                )
+
+              ],
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blueAccent,
         onPressed: (){
-          Navigator.pushNamed(context, '/chat_protocolo',arguments: {'protocolo':protocolo,'email':email});
+          Navigator.pushNamed(context, '/chat_protocolo',arguments: {'protocolo':protocolo,'email':usuario_email});
         },
-        elevation: 10, child: const Icon(Icons.chat, color: Colors.white,),
+        elevation: 10,
         hoverColor: Colors.indigoAccent,
         tooltip: "Ver acompanhamentos",
+        child: const Icon(Icons.chat, color: Colors.white,),
       ),
     );
   }
